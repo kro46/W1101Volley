27974a4a2bae785a5f00bbf4caa25030c4f95f2f
kst1040@gmail.com <kst1040@gmail.com>
2022-11-16 13:49:14 +0900

    ArrayList로 영화 목록 불러오기 완성

--- app/src/main/java/kr/ac/kumoh/s20170187com/example/w1101volley/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170187com/example/w1101volley/MainActivity.kt
@@ -1,18 +1,23 @@
 package kr.ac.kumoh.s20170187com.example.w1101volley
 
+import android.icu.number.Notation.simple
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.s20170187com.example.w1101volley.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var queue: RequestQueue
+    private val movies = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,13 +31,31 @@ class MainActivity : AppCompatActivity() {
                 url,
                 null,
                 {
-                    Toast.makeText(application,it.toString(),Toast.LENGTH_LONG).show()
+                    movies.clear()
+                    parseJson(it)
+                    Toast.makeText(application,movies.toString()
+                    .replace(",", "\n")
+                    .replace("[\\[\\]]".toRegex(), ""),Toast.LENGTH_LONG).show()
+                    binding.listMovies.adapter = ArrayAdapter<String>(
+                        this,android.R.layout.simple_list_item_1,movies)
                 },
                 {
-                    Toast.makeText(application,it.toString(),Toast.LENGTH_LONG).show()
+                    Toast.makeText(application,movies.toString(),Toast.LENGTH_LONG).show()
                 }
             )
             queue.add(request)
         }
     }
+    private fun parseJson(obj: JSONObject) {
+        val data = obj.getJSONObject("data")
+        val items = data.getJSONArray("movies");
+
+        for(i in 0 until items.length()){
+            val item: JSONObject = items[i] as JSONObject
+            val title = item.getString("title_long")
+            movies.add(title)
+        }
+    }
+
+
 }
